98359ca991fa0a6a99b60b9688155dd8a7c0061a
michaelgathara <mikegtrm@gmail.com>
2022-06-29 22:40:41 -0500

    attempted to add wandb into the project, only got the confusion matrix working but because we can make the text data into numerical data

--- LogRegression.py
+++ LogRegression.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import numpy as np
-
+import os, wandb
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+
+choice = input("Do you have the W&B API key?: (y or n): ")
+choice = choice.lower()
+if choice == 'y':
+    print("Make sure you did 'pip install wandb' ")
+    print("Going to run wandb login and you can paste your API key right in the ")
+    os.system("wandb login")
+    
+    # Going to init the project with a new run 
+    wandb.init(project="Nasa Tracked Objects", entity="computer-talkers")
 
 # Create a classifier object to perform Logistic Regression tasks
 lr = LogisticRegression()
@@ -26,6 +36,13 @@ Features are the predictive variables. In our case they are the est_diameter_min
 relative_velocity(4), miss_distance(5), orbiting_body(6), sentry_object(7), and absolute_magnitude(8) columns. 
 Note: Because orbiting_body(6) is not a numerical value we are not going to use it in the logistic regression model.
 Targets are the response variables. In our case its the hazardous column (9)./
+
+In general terms: 
+training_features = y_train
+training_targets = x_train
+
+testing_features = y_test
+testing_targets = x_test
 '''
 training_features = train.iloc[:, [2, 3, 4, 5, 7, 8]]  # all rows and columns 3 - 9 in the training data set
 training_targets = train["hazardous"]  # Training targets is a Series data type and not a DataFrame data type
@@ -33,10 +50,9 @@ training_targets = train["hazardous"]  # Training targets is a Series data type 
 testing_features = test.iloc[:, [2, 3, 4, 5, 7, 8]]  # all rows and columns 3 - 9 in the training data set
 testing_targets = test["hazardous"]  # Training targets is a Series data type and not a DataFrame data type
 print("Testing Features")
-print(testing_features.head(5))
+print(f"{testing_features.head(5)}\n")
 print("Testing Targets")
-print(testing_targets.head(5))
-
+print(f"{testing_targets.head(5)}\n")
 
 '''
 Now we are going to fit our logistic regression model and the see the fit score for the training and testing datasets.
@@ -46,19 +62,26 @@ Testing fit score:  0.9029612505504183
 Meaning the model got a score of 90.3/100 cases correctly labeled
 '''
 lr.fit(training_features, training_targets)
-training_score = lr.score(training_features, training_targets)
-print("Training fit score: ", training_score)
 
+training_score = lr.score(training_features, training_targets)
+print(f"Training fit score: {training_score}\n")
 
 lr.fit(testing_features, testing_targets)
 testing_score = lr.score(testing_features, testing_targets)
-print("Testing fit score: ", testing_score)
+print(f"Testing fit score: {testing_score}\n")
 
+# Plotting regressor to wandb
+if choice == 'y':
+    # wandb.sklearn.plot_regressor(lr, training_features, training_targets, testing_features, testing_targets, model_name="Nasa Objects")
+    # wandb.sklearn.plot_regressor(lr, training_targets, testing_targets, training_features, testing_features, model_name="Nasa Tracked Objects")
+    wandb.sklearn.plot_learning_curve(lr, training_targets, training_targets)
+    # wandb.sklearn.plot_class_proportions(training_features, testing_features)
 '''
 Log-Odds / Logits / Odds Ratio
 Google this to find its meaning in logistic/ linear regression
 '''
-print(np.transpose(lr.coef_))
+print("Log-Odds")
+print(f"{np.transpose(lr.coef_)}\n")
 
 '''
 Confusion Matrix
@@ -68,10 +91,16 @@ Ex:
 [[ cp fp]
  [ fn  cn]]
 '''
-training_conf_matrix = confusion_matrix(lr.predict(training_features), training_targets)
-print("Training Confusion matrix")
-print(training_conf_matrix)
+training_features_pred = lr.predict(training_features)
+training_conf_matrix = confusion_matrix(training_features_pred, training_targets)
 
-testing_conf_matrix = confusion_matrix(lr.predict(testing_features), testing_targets)
+print("Training Confusion matrix")
+print(f"{training_conf_matrix}\n")
+
+testing_features_pred = lr.predict(testing_features)
+testing_conf_matrix = confusion_matrix(testing_features_pred, testing_targets)
+if choice == 'y':
+    wandb.sklearn.plot_confusion_matrix(testing_targets, testing_features_pred)
+    wandb.finish()
 print("Testing Confusion matrix")
-print(testing_conf_matrix)
+print(f"{testing_conf_matrix}\n")
